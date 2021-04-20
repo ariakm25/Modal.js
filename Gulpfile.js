fb24abe78d0cc8e2b4d7cd11b4af08ca34ee2b60
Aria <45255650+ariakm25@users.noreply.github.com>
2021-04-21 01:31:40 +0700

    Delete Gulpfile.js

--- Gulpfile.js
+++
@@ -1,8 +0,0 @@
-const { src, dest } = require('gulp');
-const sass = require('gulp-sass');
-
-exports.default = () => {
-  return src('sass/modal.scss')
-  .pipe(sass())
-  .pipe(dest('./build/css/'));
-};
